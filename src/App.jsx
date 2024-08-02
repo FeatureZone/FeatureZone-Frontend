fde1ab779d9b4925f32d27d8f17a210be9ad0086
AJ-Appiah <ericapph@gmail.com>
2024-08-02 11:33:00 +0000

    CodePage UI development

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/login';
+import Register from './pages/register';
+import LandingPage from './pages/landingPage/landingPage';
+import DetailedSnippet from './pages/codePage/detailedSnippet';
+import CodePage from './pages/codePage/codePage';
 
-import LandingPage from "./assets/landingPage/landingPage"
-import Login from "./pages/login";
-import Register from "./pages/register";
+
+const snippets = [
+  // Your snippet data here
+];
 
 const router = createBrowserRouter([
-  { path: "/", element: <LandingPage /> },
-    {
-      path: "/login",
-      element: <Login />
-    },
-
-    {
-      path: "/register",
-      element: <Register />
-    }
+  {
+    path: '/',
+    element: <LandingPage snippets={snippets} />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/code',
+    element: <CodePage snippets={snippets} />,
+  },
+  {
+    path: '/code/:id',
+    element: <DetailedSnippet snippets={snippets} />,
+  },
 ]);
 
 function App() {
-return ( <RouterProvider router={router} /> );
-
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
