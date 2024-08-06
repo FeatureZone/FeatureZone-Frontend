1dcc23043a407d73924b88bc8c942d06f8233069
rtdongullo <dongromert@gmail.com>
2024-08-06 09:16:19 +0000

    committing package duplication and router conflict

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,30 @@ import Register from './pages/register';
 import LandingPage from './pages/landingPage/landingPage';
 import DetailedSnippet from './pages/codePage/detailedSnippet';
 import CodePage from './pages/codePage/codePage';
+import ProfilePage from './pages/profile';
 
 
 const snippets = [
   // Your snippet data here
 ];
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <LandingPage snippets={snippets} />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <LandingPage snippets={snippets} />,
+//   },
+//   {
+//     path: '/login',
+//     element: <Login />,
+//   },
+// ])
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import LandingPage from './assets/landingPage/landingPage';
-import Login from './pages/login';
-import Register from './pages/register';
-import ProfilePage from './pages/profile';
 
 const router = createBrowserRouter([
-  { path: '/', element: <LandingPage /> },
+  { path: '/', 
+    element: <LandingPage />,
+   },
   {
     path: '/login',
     element: <Login />,
